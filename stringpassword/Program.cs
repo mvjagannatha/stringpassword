eb0651453fbe619a4033ecdc37a8913cb6b1ef96
Admin <Admin@DESKTOP-6PQE64G>
2024-01-15 08:10:40 +0530

    Add project files.

---
+++ stringpassword/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        const string loginid = "Admin";
+        const string password= "password";
+        int maxattempts = 3;
+        int attempts = 0;
+        while(attempts<maxattempts)
+        {
+            Console.WriteLine("enter loginid");
+            string enteredlogin = Console.ReadLine();
+            Console.WriteLine("Enter password: ");
+            string enteredpassword=Console.ReadLine();
+            if(enteredlogin==loginid && enteredpassword==password)
+            {
+                Console.WriteLine("login successful");
+                break;
+            }
+            else
+            {
+                attempts++;
+                Console.WriteLine(" incorrect credentials:,no of attempts left={0}", (maxattempts-attempts));
+            }
+
+        }
+        if(attempts==maxattempts)
+        {
+            Console.WriteLine("max attempts reached: your account is blocked");
+        }
+    }
+}
